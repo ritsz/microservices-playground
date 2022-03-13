ad0c63d4b15f9f75495a70e8f3b8ff05cbe9c351
Ritesh Ranjan <r.ranjan789@gmail.com>
2022-03-14 01:21:22 +0530

    Move to envoy base image for frontproxy

--- 6-todo-service/quick-test.sh
+++ 6-todo-service/quick-test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-curl -v -X POST 192.168.1.36:8080/users/rritesh
+curl -m 2 -v -X POST localhost:8080/users/rritesh
 
-curl -v -X GET https://192.168.1.36:8443/users/rritesh --insecure | jq .
+curl -m 2 -v -X GET https://localhost:8443/users/rritesh --insecure | jq .
+
+curl -m 2 -v -X DELETE https://localhost:8443/users/rritesh --insecure
+
+curl -m 2 -v -X GET http://localhost:8080/users/rritesh | jq .
