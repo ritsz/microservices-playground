da88c61a2b35060151f65b050411c92891211033
Ritesh Ranjan <r.ranjan789@gmail.com>
2022-10-30 18:16:01 +0530

    Add listener script

--- 7-fileanalysis/utils/kafka-listener-script.sh
+++ 7-fileanalysis/utils/kafka-listener-script.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-/kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic postgres-database.public.file_entity  --from-beginning
+/kafka/bin/kafka-console-consumer.sh              \
+    --bootstrap-server kafka:9092                 \
+    --topic postgres-database.public.file_entity  \
+    --from-beginning
