275ba8fe8dc033465448a5fc3769892102cd2f2c
Ritesh Ranjan <r.ranjan789@gmail.com>
2022-11-01 03:03:19 -0700

    Fixup

--- 7-fileanalysis/launch-2.bash
+++ 7-fileanalysis/launch-2.bash
@@ -10,5 +10,5 @@ mkdir /mnt2/data-2
 echo "Build images"
 echo "Start containers"
 
-docker compose -f docker-compose.yaml --env-file ev.config up -d --force-recreate --build
+docker compose -f docker-compose-2.yaml --env-file env.config up -d --force-recreate --build
 set +x
